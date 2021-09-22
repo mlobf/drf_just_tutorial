b570887321e640d2c79cb326845595c9fc2d4db7
mlobf <marcoslemeborbafilho@gmail.com>
2021-09-21 22:50:35 -0300

    Testing commit using vim

--- tutorial/tutorial/quickstart/serializers.py
+++ tutorial/tutorial/quickstart/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-
+# testing commit by vim
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
